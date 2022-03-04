42659255b20006ba89ed7282af5c6ca5687927a6
Wanjiru-Kamau <njirukmaureen@gmail.com>
2022-03-04 19:53:38 +0300

    kotlin strings assignment

---
+++ src/main.kt
@@ -0,0 +1,55 @@
+fun main() {
+    var college = "akirachix"
+    println(college[0].toString()+college[2]+college[3])
+    greeting()
+    intrest()
+    name()
+}
+fun greeting(){
+    var name = "maureen"
+    var age = 19
+    var statement = ("Hi my name is $name and i am $age years old")
+    println(statement)
+}
+fun intrest(){
+    var intrestingfact = "i love food"
+    println(intrestingfact . length)
+
+}
+
+fun name(){
+    var name = "maureen"
+    if (name.equals("maureen")){
+        println("that's me")
+    }else{
+        println("i don't know who that is")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
